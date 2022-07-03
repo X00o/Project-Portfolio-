8e349c7765a0ade2eed880fd0e432a9eada44ccc
souvik <souvikbasak05@gmail.com>
2022-07-03 18:08:31 +0530

    init the file

---
+++ script.js
@@ -0,0 +1,9 @@
+
+        // function playVideo() {
+        //     $('#sample_video').trigger('play');
+        // }
+        // onclick=" pauseVideo()" 
+        // function pauseVideo() {
+        //     $('#sample_video').trigger('pause');
+        // }
+     
